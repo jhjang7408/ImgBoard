5615e1614409f1f15dc271c54b00c16003a84ffc
jhjang7408 <jhjang7408@naver.com>
2023-12-26 17:59:31 +0900

    static에 저장된 사진 잘 불러온다

--- src/main/java/com/temisone/imgboard/controller/BoardController.java
+++ src/main/java/com/temisone/imgboard/controller/BoardController.java
@@ -4,12 +4,14 @@ import com.temisone.imgboard.entity.BoardEntity;
 import com.temisone.imgboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,6 +37,16 @@ public class BoardController {
         boardService.write(boardEntity, file);
 
         return "redirect:/board";
+    }
+
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        List<BoardEntity> list = boardService.findAll();
+
+        model.addAttribute("list", list);
+
+        return "list";
     }
 
 }
--- src/main/java/com/temisone/imgboard/service/BoardService.java
+++ src/main/java/com/temisone/imgboard/service/BoardService.java
@@ -8,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -33,6 +34,11 @@ public class BoardService {
         boardEntity.setImgpath("/" + imgname);
 
         boardRepository.save(boardEntity);
+    }
+
+
+    public List<BoardEntity> findAll(){
+        return boardRepository.findAll();
     }
 
 
