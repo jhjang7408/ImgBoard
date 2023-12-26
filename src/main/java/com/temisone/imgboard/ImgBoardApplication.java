7889da714f52c061747957f2a689502b3bbbe22c
jhjang7408 <jhjang7408@naver.com>
2023-12-26 14:31:38 +0900

    이미지 게시판 만들어보기1

---
+++ src/main/java/com/temisone/imgboard/ImgBoardApplication.java
@@ -0,0 +1,13 @@
+package com.temisone.imgboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/temisone/imgboard/ImgBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temisone.imgboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
